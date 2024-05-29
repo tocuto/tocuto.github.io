8192a0699ddaf49a84358b85325c1e5b41f0162a
Tocutoeltuco <tocutoeltuco@gmail.com>
2024-05-29 18:56:23 -0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import routify from "@roxi/routify/vite-plugin";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), routify({})],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@use \"src/variables.scss\" as *;"
+      }
+    }
+  }
+})
